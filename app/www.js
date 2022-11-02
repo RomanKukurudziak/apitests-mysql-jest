08b4e3720a196ecbaae11d150e0ce152e89c229d
Roman Kukurudziak <romankukurudziak@Romans-MacBook-Pro.local>
2022-11-02 23:32:26 +0200

    add read, delete and update operations

--- app/www.js
+++ app/www.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const Hero = require('./models/hero.model');
- 
+
 const jsonParser = bodyParser.json();
 
 app.use(jsonParser);
@@ -12,6 +12,36 @@ app.get('/heroes', async (req, res) => {
 
   const list = await Hero.findAll();
   res.send({ list });
+});
+
+app.get('/heroes/:id', async (req, res) => {
+  await Heroes.sync();
+
+  const hero = await Hero.findOne({ where: {id: req.params.id } });
+
+  res.send({ data: hero })
+});
+
+app.put('/heroes/:id', async (req, res) => {
+  await Hero.sync();
+
+  const updatedHero = await Hero.update(
+    {
+      name: req.body.name,
+      ownerId: req.body.ownerId,
+    },
+    { where: { id: req.params.id } },
+  );
+
+  res.send({ data: updatedHero });
+});
+
+app.delete('/heroes/:id', async (req, res) => {
+  await Hero.sync();
+
+  const deleted = await Hero.destroy({ where: { id: req.params.id } })
+
+  res.send({ success: Boolean(deleted) });
 });
 
 app.post('/heroes', async (req, res) => {
@@ -27,4 +57,4 @@ app.post('/heroes', async (req, res) => {
 
 app.listen(8080, () => {
   console.log(`Example app listening on port 8080`);
-})
+});
